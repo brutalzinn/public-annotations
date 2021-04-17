e9cd5aab8f34de5ce6ab90cc0ef0c5594d46a8da
robertocpaes <31475696+brutalzinn@users.noreply.github.com>
2021-04-17 18:40:55 -0300

    Now the code has very simple and clean
    I added a website from where i learned the algorithmium of CPF

--- cpfChecker.js
+++ cpfChecker.js
@@ -1,57 +1,40 @@
-//now os working
-function checkCpf(cpf){
-  var soma = 0
-  var chCheck = false
-    for(var d = 0;d < cpf.length;d++){
-  if(cpf[d+1] && cpf[d] == cpf[d +1]){
-  chCheck = true
-  }else if ( cpf[d+1] && cpf[d] != cpf[d+1]){
-  	chCheck = false
-  }
-  }
-  console.log(chCheck)
-  if(chCheck){
-  	return false
-  }
-  console.log(chCheck)
-  var verificardois = cpf[cpf.length - 1]
-    var verificarum = cpf[cpf.length - 2]
-console.log(verificardois,verificarum)
-  
-  var multiple = 10
-  for(var d = 0;d < cpf.length - 2;d++){
-   soma += parseInt(cpf[d]) * multiple
-    multiple--
-  }
-  
-  var resto = (soma * 10) % 11
-  if(resto == 10){
-  	resto = 0
-  }
-  if(resto != verificarum){
-  	return false 
-  }
- if(resto == 0 || resto == 1){
-   primeirodigito = 0
- }else{
-   primeirodigito = 11 - resto
- }
-     multiple = 11
-     soma = 0
-      for(var d = 0;d < cpf.length - 1;d++){
-   soma += parseInt(cpf[d]) * multiple
-    multiple--
-  }
-   
-resto = (soma * 10) % 11
-    if( resto == 10){
-    	resto = 0
+//Validador de cpf 17/04/2021 - cálculo baseado nas informações contidas em http://clubes.obmep.org.br/blog/a-matematica-nos-documentos-cpf/
+function checkCpf(cpf) {
+    var chCheck = false
+    for (var d = 0; d < cpf.length; d++) {
+        if (cpf[d + 1] && cpf[d] == cpf[d + 1]) {
+            chCheck = true
+        } else if (cpf[d + 1] && cpf[d] != cpf[d + 1]) {
+            chCheck = false
+        }
     }
-    if(resto != verificardois){
-    	return false 
-    }  
-    
+    if (chCheck) {
+        return false
+    }
+    var digitodois = cpf[cpf.length - 1]
+    var digitoum = cpf[cpf.length - 2]
+    const verifyNumber = (multiple, digit = 2) => {
+        var soma = 0
+        for (var d = 0; d < cpf.length - digit; d++) {
+            soma += parseInt(cpf[d]) * multiple
+            multiple--
+        }
+        return (soma * 10) % 11
+    }
+    var resto = verifyNumber(10)
+    if (resto == 10) {
+        resto = 0
+    }
+    if (resto != digitoum) {
+        return false
+    }
+    resto = verifyNumber(11, 1)
+    if (resto == 10) {
+        resto = 0
+    }
+    if (resto != digitodois) {
+        return false
+    }
     return true
 }
-    
-console.log(checkCpf('11111111111'))
+console.log(checkCpf('000000000'))
